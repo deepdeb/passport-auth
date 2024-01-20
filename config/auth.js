6d3767e5b3f28510d489952e68b4daaaca9c811a
deepdeb <debnath.pradeep619@gmail.com>
2024-01-21 00:36:25 +0530

    Initial commit

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash("error_msg", "Please Sign in");
+    res.redirect("/users/login");
+  },
+};
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://debnathpradeep619:mWFD8LX1YQfQ1WBa@pradeep-test-db.rg5dh8o.mongodb.net/?retryWrites=true&w=majority",
+};
