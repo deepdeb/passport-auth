fc535e822a089626c5ae468d7feb433625e190d2
Pradeep Debnath <debnath.pradeep619@gmail.com>
2024-03-29 09:48:25 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://debnathpradeep619:mWFD8LX1YQfQ1WBa@pradeep-test-db.rg5dh8o.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://your-mongo-database-credential.mongodb.net/?retryWrites=true&w=majority",
 };
